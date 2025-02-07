7951dfc52568b064c1c3be16ed3b7ee9e98d19e2
Samuu <samuelfilipevianadois@gmail.com>
2025-02-06 23:50:03 -0300

    feat: add translate words

--- app/src/main/java/com/samuel/technical_test_agiletv/ui/theme/Type.kt
+++ app/src/main/java/com/samuel/technical_test_agiletv/ui/theme/Type.kt
@@ -2,14 +2,20 @@ package com.samuel.technical_test_agiletv.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.samuel.technical_test_agiletv.R
 
+
+val CustomFontFamily = FontFamily(
+  Font(R.font.poppins_regular, FontWeight.Normal)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
   bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
+    fontFamily = CustomFontFamily,
     fontWeight = FontWeight.Normal,
     fontSize = 16.sp,
     lineHeight = 24.sp,
